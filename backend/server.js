75436ac0b0dc08df236b3f3a732f95fd745def46
Syed Abeer Bin Haider <mcareer259@gmail.com>
2025-05-10 00:31:09 +0500

    Updated backend and restructured frontend with vercel config

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const dotenv = require('dotenv');
 const projectRoutes = require('./routes/projectRoutes');
 const authRoutes = require('./routes/authRoutes');
 const contactRoutes = require('./routes/contactRoutes');
+const path = require("path")
 
 dotenv.config();
 
@@ -14,12 +15,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
 // MongoDB Connection
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/portfolio')
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
-// Routes
+// Routes|
+
+app.get("/", (req, res) => {
+  app.use(express.static(path.resolve(__dirname, "frontend", "dist")));
+  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+})
+
 app.use('/api/projects', projectRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/contact', contactRoutes);
